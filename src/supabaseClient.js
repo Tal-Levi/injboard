13c342436878638f47909f1526b8a5bc15cf7699
cx-tal-levi <20534588+Tal-Levi@users.noreply.github.com>
2025-07-11 09:53:06 +0300

    tst

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ahmtmwnqijeshxlrxjbg.supabase.co';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('Missing Supabase environment variables');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
